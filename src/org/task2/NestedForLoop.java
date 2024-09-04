d2bdaa82a8f16764efab3e7761290f10027a0d9b
Pranesh17-Tester <praneshsv17@gmail.com>
2024-09-04 13:22:29 +0530

    this my project

---
+++ src/org/task2/NestedForLoop.java
@@ -0,0 +1,24 @@
+package org.task2;
+
+// Nested For loop
+ 
+// j
+
+public class NestedForLoop {
+
+	public static void main(String[] args) {
+		
+		
+	for (int i = 3; i <= 7; i++) {
+		
+	for (int j = 2; j <= 5; j++) {
+	
+		System.out.println(j);
+			
+	}
+		
+	}	
+		
+	}
+	
+    }
---
+++ src/org/task2/PracticeNestedForLoop.java
@@ -0,0 +1,16 @@
+package org.task2;
+
+public class PracticeNestedForLoop {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 3; i++) {
+		      for (int j = 1; j <= 3; j++) {
+		        System.out.println(i);
+		      }
+
+		    }
+
+	}
+
+}
